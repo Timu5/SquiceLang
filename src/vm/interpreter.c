20dd14b289fd59b3140d188e5a0536bcd5c2bbee
Timu5 <timusziomek@gmail.com>
2020-05-09 16:18:48 +0200

    Add exec function

--- src/vm/interpreter.c
+++ src/vm/interpreter.c
@@ -117,6 +117,124 @@ void dis()
     ip = 0;
 }
 
+void exec(ctx_t *global, char * opcodes, int size)
+{
+    ctx_t *context = global;
+    while (1)
+    {
+        if (ip >= size)
+            break;
+        int byte = opcodes[ip];
+        ip += 1;
+
+        switch (byte)
+        {
+        case O_NOP:
+            break;
+        case O_PUSHN:
+            vector_push(global->stack, value_number(getint()));
+            break;
+        case O_PUSHS:
+            vector_push(global->stack, value_string(getstr()));
+            break;
+        case O_PUSHV:
+            vector_push(global->stack, ctx_getvar(context, getstr()));
+            break;
+        case O_POP:
+            vector_pop(global->stack);
+            break;
+        case O_STOREFN:
+            ctx_addfn(context, getstr(), (int)vector_pop(global->stack)->number, NULL);
+            break;
+        case O_STORE:
+            ctx_addvar(context, getstr(), vector_pop(global->stack));
+            break;
+        case O_UNARY:
+        {
+            value_t *a = vector_pop(global->stack);
+            vector_push(global->stack, value_unary(getint(), a));
+            break;
+        }
+        case O_BINARY:
+        {
+            value_t *b = vector_pop(global->stack);
+            value_t *a = vector_pop(global->stack);
+            vector_push(global->stack, value_binary(getint(), a, b));
+            break;
+        }
+        case O_CALL:
+        case O_CALLM:
+            value_t *fn_value = vector_pop(global->stack);
+            while (fn_value->type == V_REF)
+                fn_value = fn_value->ref;
+            if (fn_value->type != V_FN)
+            {
+                throw("Can only call functions!");
+            }
+            if (fn_value->fn->native != NULL)
+            {
+                fn_value->fn->native(global);
+            }
+            else
+            {
+                context = ctx_new(context);
+                if (byte == O_CALLM)
+                {
+                    ctx_addvar(context, "this", vector_pop(global->stack)); // add "this" variable
+                }
+                vector_push(call_stack, ip);
+                //vector_pop(global->stack);
+                ip = fn_value->fn->address;
+            }
+            break;
+        case O_RETN:
+            vector_push(global->stack, value_null());
+        case O_RET:
+            if (vector_size(call_stack) == 0)
+            {
+                throw("Nothing to return from!");
+                return;
+            }
+            int ret_adr = vector_pop(call_stack);
+            ip = ret_adr;
+            if (context->parent != NULL)
+                context = context->parent;
+            break;
+        case O_JMP:
+            ip = getint();
+            break;
+        case O_BRZ:
+            value_t *v = vector_pop(global->stack);
+            int nip = getint();
+            if (v->number == 0)
+                ip = nip;
+            break;
+        case O_INDEX:
+        {
+            value_t *expr = vector_pop(global->stack);
+            value_t *var = vector_pop(global->stack);
+            vector_push(global->stack, value_get((int)expr->number, var));
+            break;
+        }
+        case O_MEMBER:
+        {
+            value_t *var = vector_pop(global->stack);
+            char *name = getstr();
+            vector_push(global->stack, value_member(name, var));
+            break;
+        }
+        case O_MEMBERD:
+        {
+            value_t *var = vector_pop(global->stack);
+            char *name = getstr();
+            vector_push(global->stack, var);
+            vector_push(global->stack, value_member(name, var));
+            break;
+        }
+        }
+    }
+}
+
 int main(int argc, char ** argv)
 {
     if (argc < 2)
@@ -136,123 +254,10 @@ int main(int argc, char ** argv)
 
     global = ctx_new(NULL);
     builtin_install(global);
-    ctx_t *context = global;
 
     try
     {
-        while (1)
-        {
-            if (ip >= fsize)
-                break;
-            int byte = opcodes[ip];
-            ip += 1;
-
-            switch (byte)
-            {
-            case O_NOP:
-                break;
-            case O_PUSHN:
-                vector_push(global->stack, value_number(getint()));
-                break;
-            case O_PUSHS:
-                vector_push(global->stack, value_string(getstr()));
-                break;
-            case O_PUSHV:
-                vector_push(global->stack, ctx_getvar(context, getstr()));
-                break;
-            case O_POP:
-                vector_pop(global->stack);
-                break;
-            case O_STOREFN:
-                ctx_addfn(context, getstr(), (int)vector_pop(global->stack)->number, NULL);
-                break;
-            case O_STORE:
-                ctx_addvar(context, getstr(), vector_pop(global->stack));
-                break;
-            case O_UNARY:
-            {
-                value_t *a = vector_pop(global->stack);
-                vector_push(global->stack, value_unary(getint(), a));
-                break;
-            }
-            case O_BINARY:
-            {
-                value_t *b = vector_pop(global->stack);
-                value_t *a = vector_pop(global->stack);
-                vector_push(global->stack, value_binary(getint(), a, b));
-                break;
-            }
-            case O_CALL:
-            case O_CALLM:
-                value_t *fn_value = vector_pop(global->stack);
-                while (fn_value->type == V_REF)
-                    fn_value = fn_value->ref;
-                if (fn_value->type != V_FN)
-                {
-                    throw("Can only call functions!");
-                }
-                if (fn_value->fn->native != NULL)
-                {
-                    fn_value->fn->native(global);
-                }
-                else
-                {
-                    context = ctx_new(context);
-                    if (byte == O_CALLM)
-                    {
-                        ctx_addvar(context, "this", vector_pop(global->stack)); // add "this" variable
-                    }
-                    vector_push(call_stack, ip);
-                    //vector_pop(global->stack);
-                    ip = fn_value->fn->address;
-                }
-                break;
-            case O_RETN:
-                vector_push(global->stack, value_null());
-            case O_RET:
-                if (vector_size(call_stack) == 0)
-                {
-                    throw("Nothing to return from!");
-                    return;
-                }
-                int ret_adr = vector_pop(call_stack);
-                ip = ret_adr;
-                if (context->parent != NULL)
-                    context = context->parent;
-                break;
-            case O_JMP:
-                ip = getint();
-                break;
-            case O_BRZ:
-                value_t *v = vector_pop(global->stack);
-                int nip = getint();
-                if (v->number == 0)
-                    ip = nip;
-                break;
-            case O_INDEX:
-            {
-                value_t *expr = vector_pop(global->stack);
-                value_t *var = vector_pop(global->stack);
-                vector_push(global->stack, value_get((int)expr->number, var));
-                break;
-            }
-            case O_MEMBER:
-            {
-                value_t *var = vector_pop(global->stack);
-                char *name = getstr();
-                vector_push(global->stack, value_member(name, var));
-                break;
-            }
-            case O_MEMBERD:
-            {
-                value_t *var = vector_pop(global->stack);
-                char *name = getstr();
-                vector_push(global->stack, var);
-                vector_push(global->stack, value_member(name, var));
-                break;
-            }
-            }
-        }
+        exec(global, opcodes, fsize);
     }
     catch
     {
