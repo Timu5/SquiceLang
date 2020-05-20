68ae641e8601489b31f69b388309f85752ff10cc
Timu5 <timusziomek@gmail.com>
2020-05-20 21:19:50 +0200

    Remove interpreter from main code

--- src/main.c
+++ src/main.c
@@ -14,45 +14,6 @@
 #include "parser.c"
 #include "utils.c"
 
-char input_buffer[255];
-
-int balanced()
-{
-    int i = 0;
-    int par = 0;
-    int bra = 0;
-    int squ = 0;
-    while (input_buffer[i] != '\0')
-    {
-        if (input_buffer[i] == '(')
-            par++;
-        if (input_buffer[i] == ')')
-            par--;
-        if (input_buffer[i] == '[')
-            squ++;
-        if (input_buffer[i] == ']')
-            squ--;
-        if (input_buffer[i] == '{')
-            bra++;
-        if (input_buffer[i] == '}')
-            bra--;
-        i++;
-    }
-    return par == 0 && bra == 0 && squ == 0;
-}
-
-void getstring()
-{
-    printf(">>> ");
-    fgets(input_buffer, 255, stdin);
-
-    while (!balanced())
-    {
-        printf("... ");
-        fgets(input_buffer + strlen(input_buffer), 255, stdin);
-    }
-}
-
 void sl_eval_str(sl_ctx_t *ctx, char *code)
 {
     sl_parser_t *parser = sl_parser_new(code);
@@ -67,27 +28,26 @@ void sl_eval_str(sl_ctx_t *ctx, char *code)
     sl_exec(ctx, bin->block, bin->size);
 }
 
-int main(int argc, char **argv)
+void sl_dis_str(sl_ctx_t *ctx, char *code)
 {
-    sl_ctx_t *ctx = sl_ctx_new(NULL);
-    sl_builtin_install(ctx);
+    sl_parser_t *parser = sl_parser_new(code);
+    sl_node_t *tree = sl_parse(parser);
+    sl_parser_free(parser);
 
-    while (1)
+    sl_binary_t *bin = sl_binary_new();
+    tree->codegen(tree, bin);
+    sl_node_free(tree);
+
+    sl_bytecode_fill(bin);
+    
+    for(int i = 0; i < bin->size; i++)
     {
-        try
-        {
-            getstring();
-            sl_eval_str(ctx, input_buffer);
-        }
-        catch
-        {
-            puts(ex_msg);
-        }
+        printf("%02hhx ", bin->block[i]);
     }
-
-    //fclose(input);
-
-    return 0;
+    printf("\n");
+    dis(bin->block, bin->size);
+    sl_exec(ctx, bin->block, bin->size);
 }
+
 
 #endif
---
+++ tools/interpreter.c
@@ -0,0 +1,62 @@
+#include "SquiceLang.h"
+#include "main.c"
+
+char input_buffer[255];
+
+int balanced()
+{
+    int i = 0;
+    int par = 0;
+    int bra = 0;
+    int squ = 0;
+    while (input_buffer[i] != '\0')
+    {
+        if (input_buffer[i] == '(')
+            par++;
+        if (input_buffer[i] == ')')
+            par--;
+        if (input_buffer[i] == '[')
+            squ++;
+        if (input_buffer[i] == ']')
+            squ--;
+        if (input_buffer[i] == '{')
+            bra++;
+        if (input_buffer[i] == '}')
+            bra--;
+        i++;
+    }
+    return par == 0 && bra == 0 && squ == 0;
+}
+
+void getstring()
+{
+    printf(">>> ");
+    fgets(input_buffer, 255, stdin);
+
+    while (!balanced())
+    {
+        printf("... ");
+        fgets(input_buffer + strlen(input_buffer), 255, stdin);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    sl_ctx_t *ctx = sl_ctx_new(NULL);
+    sl_builtin_install(ctx);
+
+    while (1)
+    {
+        try
+        {
+            getstring();
+            sl_dis_str(ctx, input_buffer);
+        }
+        catch
+        {
+            puts(ex_msg);
+        }
+    }
+
+    return 0;
+}
